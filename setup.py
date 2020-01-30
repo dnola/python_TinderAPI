c2755bc85022e5acf09e609b4bf5ea47b022c660
dnola <d.nola@me.com>
2020-01-30 15:46:06 -0800

    adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Tinder Python API',
+   version='0.01',
+   description='A simple interface for the 2020 Tinder API (Forked from MMcintire96s interface)',
+   author='David Nola',
+   author_email='david.nola.ca@gmail.com',
+   packages=['tinder_api'],
+   install_requires=['requests'], 
+)
